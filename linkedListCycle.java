8f44bec56f5da7b4fc12c7f985f8ba67d601b3de
kxinhao <kxinhao@gmail.com>
2023-11-18 01:54:35 +0800

    linked list cycle solution using 2 pointers at diff speed

---
+++ linkedListCycle.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // dual pointer travelling at diff speed to loop and meet if cycle is present
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) {
+            return false;
+        }
+        
+        ListNode curr = head;
+        ListNode skip = head.next;
+
+        while(skip!=null && skip.next!=null) {
+            if(curr == skip){
+                return true;
+            }
+            curr = curr.next;
+            skip = skip.next.next;
+        }
+        return false;
+
+    }
+} 
