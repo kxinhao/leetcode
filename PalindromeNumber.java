618f0b7d2d9b48a45580e258daf02bb57eed62bf
kxinhao <kxinhao@gmail.com>
2023-11-22 00:25:57 +0800

    added both recursive and iterative methods

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,5 +1,9 @@
 class Solution {
+    static int reverse = 0; //leetcode does not clear static var after each testcase, need to reset manually
+
     public boolean isPalindrome(int x) {
+
+        /* iterative while (avg runtime: 4ms, memory: 43MB)
         int number = x, remainder = 0, reverse = 0;        
         // all negative values are not palindromes
         if(x<0)
@@ -14,5 +18,37 @@ class Solution {
         if(reverse-x==0)
             return true;
         return false;
+        */
+
+        // recursion method (avg runtime: 24ms, memory: 43 MB)
+        int rev = 0;
+        if(x<0)
+            return false;
+        rev = reverseNumber(x);
+        reverse = 0; //reset static var
+        if(rev - x == 0) 
+            return true;
+        return false;
+
+    }
+
+    // recursion method
+    private static int reverseNumber(int number) {
+
+        if(number == 0) {
+            System.out.println(reverse);
+            return reverse;
+        }
+        
+        if(number > 0){
+            //remainder
+            int remainder = number%10;
+            //reversed
+            reverse = reverse * 10 + remainder;
+            //original number to work through
+            reverseNumber(number/10);
+        }
+
+        return reverse; 
     }
 }
