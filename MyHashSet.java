e3486539c96f08be4579ad462099da712bd73eef
khoo xinhao <kxinhao@gmail.com>
2024-01-26 20:14:18 +0800

    improved hashset impl with array bucket and custom LL

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,39 +1,64 @@
-// 3rd impl (chaining) 
 class MyHashSet {
-    private int size;
+    class Node {
+        private int key;
+        private Node next;
+
+        public Node(int key, Node next) {
+            this.key = key;
+            this.next = next;
+        }
+    }
+
+    // chaining method
+    private static final int size = 10007;
+    private Node[] bucketArr;
     private List<List<Integer>> bucketList;
 
     public MyHashSet() {
-        size = 1000;
-        bucketList = new ArrayList<>(size);
-        for(int i = 0; i < size; i++) {
-            bucketList.add(new LinkedList<>());
-        } 
+        this.bucketArr = new Node[this.size];
     }
-    
+
     public void add(int key) {
-        int index = hash(key);
-        List<Integer> bucket = bucketList.get(index);
-        if(!bucket.contains(key)) {
-            bucket.add(key);
+        int ind = this.hash(key);
+        Node node = this.bucketArr[ind];
+        if (!this.contains(key)) {
+            node = new Node(key, this.bucketArr[ind]);
+            this.bucketArr[ind] = node;
         }
     }
-    
+
     public void remove(int key) {
-        int index = hash(key);
-        List<Integer> bucket = bucketList.get(index);
-        bucket.remove(Integer.valueOf(key));    
+        int ind = this.hash(key);
+        Node node = this.bucketArr[ind];
+        if (node == null) {
+            return;
+        }
+        if (node.key == key) {
+            this.bucketArr[ind] = node.next;
+        } else
+            for (; node.next != null; node = node.next) {
+                if (node.next.key == key) {
+                    node.next = node.next.next;
+                    return;
+                }
+            }
     }
-    
+
     public boolean contains(int key) {
-        int index = hash(key);
-        List<Integer> bucket = bucketList.get(index);
-        return bucket.contains(key);
+        int ind = this.hash(key);
+        Node node = this.bucketArr[ind];
+        while (node != null) {
+            if (node.key == key) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
     }
 
     private int hash(int key) {
         return key % size;
-    } 
+    }
 }
 
 /**
