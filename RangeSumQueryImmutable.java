39d2707609bc30b6c5f458034092c5793039c75d
khoo xinhao <kxinhao@gmail.com>
2024-02-07 15:42:47 +0800

    2nd impl of range sum query

--- RangeSumQueryImmutable.java
+++ RangeSumQueryImmutable.java
@@ -1,22 +1,23 @@
+// 2nd impl
 class NumArray {
 
-    int[] prefix;
+    int[] arr;
     public NumArray(int[] nums) {
       int n =  nums.length;
-      prefix = new int[n];
-      // init first element of prefix arr
-      prefix[0] = nums[0];
-      // populate prefix arr with cumulative sum of nums arr elements
+      arr = new int[n];
+      // init first element of arr
+      arr[0] = nums[0];
+      // populate arr with cumulative sum of nums arr elements
       // summation operation at array population time, complexity O(n)
       for(int i = 1; i < n; i++) {
-        prefix[i] = nums[i] + prefix[i-1];
+        arr[i] = nums[i] + arr[i-1];
       }
 
     }
     
     public int sumRange(int left, int right) {
       // account for left = 0 case else use summation prior to left ind
-      return prefix[right] - ((left == 0) ? 0: prefix[left-1]); 
+      return arr[right] - ((left == 0) ? 0: arr[left-1]); 
     }
 }
 
