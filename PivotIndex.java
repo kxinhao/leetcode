7b93ac8e5b8dc95b4ddcc861335f08eed6884238
khoo xinhao <kxinhao@gmail.com>
2024-02-16 19:32:13 +0800

    optimised summation and comparision to O(n)

--- PivotIndex.java
+++ PivotIndex.java
@@ -1,18 +1,21 @@
+// 2nd sol, using array summation and subtraction of indv elements
 class Solution {
     public int pivotIndex(int[] nums) {
-       for(int i = 0; i<nums.length; i++) {
-           int left = 0;
-           int right = 0;
-           for(int j = 0; j < i; j++) {
-                left += nums[j];
-           }
-           for(int k = i+1; k < nums.length; k++) {
-               right += nums[k];
-           }
-           if(left == right) {
-               return i;
-           }
-       } 
-       return -1;
+      int sum = 0;
+      for(int num : nums) {
+        sum += num;
+      }
+      int lSum = 0;
+      for(int i = 0; i < nums.length; i++) {
+        // represent movement of ind to right
+        sum -= nums[i];
+        // check if left sum == right sum and return ind if true
+        if(lSum == sum) {
+          return i;
+        }
+        // add element to left sum for next index comparison
+        lSum += nums[i];
+      }
+      return -1;
     }
 }
