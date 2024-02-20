5d8df3d5ac22f2280c5662a88d88499a9d89261c
khoo xinhao <kxinhao@gmail.com>
2024-02-20 19:42:36 +0800

    added comments

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -2,12 +2,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
+        // representing exclusion of rightmost element
         int curr = 1;
+        // populate prod of left to right excluding curr element
         for(int i = 0; i < nums.length; i++) {
             ans[i] = curr;
             curr *= nums[i];
         }
+        // representing exclusion of leftmost element
         curr = 1;
+        // layer population of right to left with existing prod arr, excluding curr elem
         for(int i = nums.length-1; i>= 0; i--) {
             ans[i] *= curr;
             curr*= nums[i];
