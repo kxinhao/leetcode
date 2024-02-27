bd72847479fba6da4e89a3f22c8ac7a99aa909ad
khoo xinhao <kxinhao@gmail.com>
2024-02-27 17:43:27 +0800

    7th impl of 2d immutable range sum query

--- RangeSumQuery2DImmutable.java
+++ RangeSumQuery2DImmutable.java
@@ -1,4 +1,4 @@
-// 6th impl
+// 7th impl
 class NumMatrix {
 
     int[][] sum;
@@ -18,7 +18,9 @@ class NumMatrix {
     
     // must be 0(1)
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        // taking total area bound by (cow2, col2) - col - row + overlap of col and row
+        // max area bound row2, col2 subtract min row1 to col2+1 and subtract min row2+1 to col1,
+        // then add back the excess subtraction:
+        // taking total area bound by (row2, col2) - col - row + overlap of col and row
         // sum[row2][col1] - sum[row1-1][col2] - sum[row2][col1-1] + sum[row1-1][col1-1];
         // because of null padding in sum array, we +1 to 2d array coords to find sum
         return sum[row2+1][col2+1] - sum[row1][col2+1] - sum[row2+1][col1] + sum[row1][col1];
