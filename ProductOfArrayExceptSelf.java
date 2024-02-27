d6e0beac30072d799ee2af5535a02bf083d6b0e5
khoo xinhao <kxinhao@gmail.com>
2024-02-27 18:25:19 +0800

    2nd impl using L to R and R to L passes, store prod progress in curr

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -1,4 +1,4 @@
-// 1st solution from ans (first pass R to L using curr * nums[i])
+// 2nd solution (using L to R + R to L pass, using curr to store curr prod prog)
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
@@ -9,13 +9,17 @@ class Solution {
             ans[i] = curr;
             curr *= nums[i]; // prep for next ans[i] left to right
         }
+        // L to R pass results ans[1,1,2,6]
+        // nums[1,2,3,4]
         // representing exclusion of rightmost element
         curr = 1;
         // layer population of right to left with existing prod arr, excluding curr elem
         for(int i = nums.length-1; i>= 0; i--) {
-            ans[i] *= curr;
-            curr *= nums[i]; // prep ans[i] right to left
+            ans[i] *= curr; // 6x1=6, 2*4=8, 1*12=12, 1*24=24
+            // formulation of curr value for index i-1 calculation of ans[i]
+            curr *= nums[i]; // prep ans[i] right to left: 1*4=4, 4*3=12, 12*2=24, 24*1=24(not used)
         }
+        // R to L pass results ans[24,12,8,6]
         return ans;
 
     }
