de086d4f6bf0a33e5b46618d3783114ebd648856
khoo xinhao <kxinhao@gmail.com>
2024-02-27 17:55:48 +0800

    4th impl, added clarifying comments

--- PivotIndex.java
+++ PivotIndex.java
@@ -1,4 +1,4 @@
-// 3rd sol, using array summation and subtraction of indv elements
+// 4th impl, using array summation and subtraction of indv elements
 class Solution {
     public int pivotIndex(int[] nums) {
       int sum = 0;
@@ -7,13 +7,14 @@ class Solution {
       }
       int lSum = 0;
       for(int i = 0; i < nums.length; i++) {
-        // represent movement of ind to right
+        // represent movement of ind to right, removal of index value from sum
         sum -= nums[i];
-        // check if left sum == right sum and return ind if true
+        // check if left sum == right sum and return ind if true,
+        // index of 0 will check against lSum of 0
         if(lSum == sum) {
           return i;
         }
-        // add element to left sum for next index comparison
+        // add element to left sum from sum for i+1 index comparison
         lSum += nums[i];
       }
       return -1;
