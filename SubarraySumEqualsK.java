e12b2a4349076bd8558361327a9a0181923e9c9b
khoo xinhao <kxinhao@gmail.com>
2024-03-01 23:54:34 +0800

    2nd impl using map to store remainder count

--- SubarraySumEqualsK.java
+++ SubarraySumEqualsK.java
@@ -1,17 +1,18 @@
-// 1st impl, brute force with O(n2)
-class Solution { 
-  public int subarraySum(int[] nums, int k) {
-      int count = 0;
-      
-      for(int i = 0; i < nums.length; i++) {
-        int sum = 0;
-        for(int j = i; j < nums.length; j++) {
-          sum += nums[j];
-          if(sum==k) {
-            count++;
-          } 
+// 2nd soln, using map to store remainder count
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0, sum = 0;
+        map.put(0,1);
+        for (int i = 0; i<nums.length; i++) {
+            sum += nums[i];
+            int remainder = sum - k;
+            if(map.containsKey(remainder)){
+                count += map.get(remainder);
+            }
+            // sum += map.getOrDefault(sum-k,0);
+            map.put(sum, map.getOrDefault(sum,0)+1);
         }
-      }
-      return count;
+        return count;
     }
 }
