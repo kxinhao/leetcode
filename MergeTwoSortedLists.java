c199d9fecd6067dacaff1f95094411f4dc9a9ef7
khoo xinhao <kxinhao@gmail.com>
2024-03-12 19:36:07 +0800

    1st impl merge sorted linked lists

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,44 @@
+// 1st impl, recursion/iterative (recursion may result in stack overflow when facing larger input)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+      /*
+        // terminal conditions
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        // recursion calls
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+      */ 
+        if(list1 == null) return list2;
+        else if(list2 == null) return list1;
+        ListNode ans = new ListNode(0);
+        ListNode curr = ans;
+        while(list1!=null && list2!=null) {
+            if(list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = (list1 == null ? list2 : list1);
+        return ans.next;
+    }
+}
