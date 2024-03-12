9e5f556deb0ed3c313018e5a465badda85d218e3
khoo xinhao <kxinhao@gmail.com>
2024-03-12 17:29:54 +0800

    6th impl

--- SubarraySumEqualsK.java
+++ SubarraySumEqualsK.java
@@ -1,4 +1,4 @@
-// 5th impl, added logic of sum - k count storage and retrieval in map
+// 6th impl, store occurences of (subarraySum - k) in map
 class Solution {
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -8,12 +8,12 @@ class Solution {
         for (int i = 0; i<nums.length; i++) {
             sum += nums[i];
             // [1,2,3] , k = 3
-            // if sum[start] = sum[end] - k,
-            // 0 = 1+2 - 3
-            // 1+2 = 1+2+3 - 3
             // k = sum[end] - sum[start] (meaning sum of vals seq from start to end = k)
             // 3 = 1+2 - 0
             // 3 = 1+2+3 - 1+2
+            // sum[start] = sum[end] - k,
+            // 0 = 1+2 - 3
+            // 1+2 = 1+2+3 - 3
             // retrieve occurences of sum[start] to find how many possible variations of deriving k
             count += map.getOrDefault(sum-k,0);
             // sum value count increment and update in map
