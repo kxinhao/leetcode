321f13fb759f4e6cbb140832d5425fc584ea124e
khoo xinhao <kxinhao@gmail.com>
2024-03-19 18:02:10 +0800

    added clarifying comments

--- BestTimeToTradeStock.java
+++ BestTimeToTradeStock.java
@@ -1,11 +1,15 @@
+// calculate profit based on current price before checking for bestBuyPrice
+// solution works as the bestProfit is checked first before bestBuyPrice is checked/replaced
 class Solution {
     public int maxProfit(int[] prices) {
       int bestProfit = 0, bestBuyPrice = prices[0];
       for(int price : prices) {
+        // calculate profit using bestBuyPrice
         int currProfit = price - bestBuyPrice;
         if(currProfit > bestProfit) {
           bestProfit = currProfit;
         }
+        // check/replace bestBuyPrice
         if(price < bestBuyPrice) {
           bestBuyPrice = price;
         }
