56a4e4c17591e39a9abb95a2c95701dfb422400f
khoo xinhao <kxinhao@gmail.com>
2024-03-19 17:56:52 +0800

    simplified code

--- BestTimeToTradeStock.java
+++ BestTimeToTradeStock.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int bestProfit = 0;
-        int bestPrice = prices[0];
-        for (int i = 0; i<prices.length; i++) {
-            // int currProfit = prices[i] - bestPrice;
-            // if(prices[i] < bestPrice) {
-            //     bestPrice = prices[i];
-            // }
-            // else if(currProfit > bestProfit) {
-            //     bestProfit = currProfit;
-            // }
-            bestProfit = Math.max(bestProfit, prices[i] - bestPrice);
-            bestPrice = Math.min(bestPrice, prices[i]);
+      int bestProfit = 0, bestBuyPrice = prices[0];
+      for(int price : prices) {
+        int currProfit = price - bestBuyPrice;
+        if(currProfit > bestProfit) {
+          bestProfit = currProfit;
         }
-        return bestProfit;
+        if(price < bestBuyPrice) {
+          bestBuyPrice = price;
+        }
+      }
+      return bestProfit;
     }
 }
