1279a98416832196c7694da095dd445df2319032
unknown <kxinhao@gmail.com>
2024-04-11 14:44:55 +0800

    impl for Balanced BT check, using recursion to count from bottom of tree

--- LowestCommonAncestorBST.java
+++ LowestCommonAncestorBST.java
@@ -1,3 +1,4 @@
+// 1st impl with recursion
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -10,9 +11,9 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // if lesser than root, check left branch
-        // if more than, check right branch
-        // return root if p and q are not on same branch
+        // if lesser than root, enter left branch
+        // if more than, enter right branch
+        // return root if p and q are not on same branch or p/q is root
         if(root.val>p.val && root.val>q.val) return lowestCommonAncestor(root.left,p,q);
         else if(root.val<p.val && root.val<q.val) return lowestCommonAncestor(root.right,p,q);
         else return root;
