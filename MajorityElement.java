3d1f709d1b910a93a79e6969bce1dad50c59eda1
unknown <kxinhao@gmail.com>
2024-04-16 19:28:54 +0800

    added comments

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,4 +1,6 @@
-// Boyer-Moore Majority Vote Algorithm
+// Boyer-Moore Majority Vote Algorithm; linear TC O(n), constant SC O(1)
+// sort and get middle element; logarithmic TC O(nlogn), constant SC O(1)
+// hashmap population; linear TC O(n), linear SC O(n)
 class Solution {
     public int majorityElement(int[] nums) {
         int majElem = nums[0], count = 1;
