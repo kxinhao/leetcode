9e7478a7c9e426f483e2969e972eee33544320be
unknown <kxinhao@gmail.com>
2024-04-19 18:26:36 +0800

    1st impl for Diameter of Binary Tree, using logic from Balanced Binary Tree soln

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,33 @@
+// soln: for each node, count left + right edges and replace largest total 
+//       distance if longer.
+// Hint: Balanced Binary Tree derivation
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+       getDiameter(root);
+       return result;
+    }
+    public int getDiameter(TreeNode root) {
+        if(root==null) return 1;
+        int l = getDiameter(root.left);
+        int r = getDiameter(root.right);
+        int dia = l+r;
+        if(result<dia) result = dia;
+        return 1+ Math.max(l,r);
+    }
+}
