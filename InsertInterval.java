4965b5273a1754fa42e089155287349be49a6734
unknown <kxinhao@gmail.com>
2024-04-30 18:00:26 +0800

    2nd impl deriving from solns

--- InsertInterval.java
+++ InsertInterval.java
@@ -1,47 +1,25 @@
 /**
  * LeetCode 57 Insert Interval (Medium)
  * brute force impl, failed at non overlap new interval test
+ * 2nd try ans derived from solutions, using arraylist and single pass index tracking operations
  */
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if(intervals==null||intervals.length==0) return new int[][] {newInterval};
-        int newLo = newInterval[0], newHi = newInterval[1];
-        int intervalCount = 0, overlapStart=0, overlapEnd=0;
-        boolean intervalIntersect = false;
-        for(int i = 0; i<intervals.length; i++) {
-            int lo = intervals[i][0], hi = intervals[i][1];
-            intervalCount++;
-            if((newLo>=lo&&newLo<=hi)||(newHi>=lo&&newHi<=hi)||(newLo<lo&&newHi>hi)){
-                intervalCount--; 
-                intervalIntersect = true;
-            } else {
-                intervalIntersect = false;
-            }
-            if(intervalIntersect) {
-                if(lo<newLo) {
-                    newLo = lo;
-                    overlapStart = i;
-                    overlapEnd = i;
-                }
-                if(hi>newHi) { 
-                    newHi = hi;
-                    overlapEnd = i;
-                }
-            }
+        List<int[]> ans = new ArrayList<>();
+        int i = 0;
+        int start = newInterval[0];
+        int end = newInterval[1];
+        // add preintersect intervals
+        while (i<intervals.length && intervals[i][1]<newInterval[0]) ans.add(intervals[i++]);
+        // add intersecting intervals
+        while (i<intervals.length && intervals[i][0]<=newInterval[1]) {
+            start = Math.min(start,intervals[i][0]);
+            end = Math.max(end,intervals[i][1]);
+            i++;
         }
-        intervalCount++;
-        int[][] ans = new int[intervalCount][2];
-        int shift = overlapEnd - overlapStart;
-        for(int i = 0; i<intervals.length;i++) {
-            if(i==overlapStart) {
-                ans[i] = new int[] {newLo,newHi};
-                i = overlapEnd;
-            } else if(i>overlapStart){
-                ans[i-shift] = intervals[i];
-            } else {
-                ans[i] = intervals[i];
-            }
-        }
-        return ans;
+        ans.add(new int[]{start,end});
+        // add post intersect intervals
+        while(i<intervals.length) ans.add(intervals[i++]);
+        return ans.toArray(new int [ans.size()][2]);
     }
 }
