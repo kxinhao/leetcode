65f911f8f58bee3dbd214365120c11df5f137802
unknown <kxinhao@gmail.com>
2024-05-03 20:07:20 +0800

    TwoSum II sorted input array impl

---
+++ TwoSumII.java
@@ -0,0 +1,15 @@
+/**
+ * LeetCode 167 Two Sum II - Input Array is Sorted (Medium)
+ * use 2 pointer
+ */
+
+class Solution {
+  public int[] twoSum(int[] numbers, int target) {
+    int l = 0, r = numbers.length-1;
+    while(numbers[l] + numbers[r] != target) {
+      if(numbers[l] + numbers[r] < target) l++;
+      else r--;
+    }
+    return new int[] {l+1,r+1};
+  }
+}
