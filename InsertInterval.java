a4fe1e44121edbe4b754fe096d8d2a389bc7779b
unknown <kxinhao@gmail.com>
2024-05-03 14:33:28 +0800

    added 2nd soln using input mutation

--- InsertInterval.java
+++ InsertInterval.java
@@ -1,8 +1,9 @@
 /**
  * LeetCode 57 Insert Interval (Medium)
  * 3 cases to account for, pre intersect of newInterval, post intersect and intersecting intervals
- * brute force impl, failed at non overlap new interval test
+ * ##brute force impl, failed at non overlap new interval test
  * 2nd try ans derived from solutions, using arraylist and single pass index tracking operations
+ * 2nd soln with input mutation
  */
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
@@ -24,5 +25,34 @@ class Solution {
         // (interval starts after end of new interval)
         while(i<intervals.length) ans.add(intervals[i++]);
         return ans.toArray(new int [ans.size()][2]);
+    }
+}
+
+// input mutation soln
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        for(int[]interval : intervals) {
+            // newInterval before interval slot, insert newInterval and replace with interval value
+            if(newInterval[1]<interval[0]) {
+                ans.add(newInterval);
+                newInterval = interval;
+            }
+            // interval before newInterval, insert interval slot
+            else if(interval[1]<newInterval[0]) {
+                ans.add(interval);
+            }
+            else {
+            // eval overlapping slots to get merged slot values
+            // newInterval[1]>=interval[0] && interval[1]>=newInterval[0]
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            }
+        }
+
+        ans.add(newInterval);
+
+        return ans.toArray(new int[ans.size()][]);
+
     }
 }
