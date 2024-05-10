ac68e9d2aaea91ed47350b3e315a7cf50a22b682
unknown <kxinhao@gmail.com>
2024-05-10 16:03:48 +0800

    added comments

--- CloneGraph.java
+++ CloneGraph.java
@@ -5,12 +5,15 @@
 
 class Solution {
     public Node cloneGraph(Node node) {
+        // map acts as seen node tracker
         public HashMap<Integer, Node> map = new HashMap<>();
         public Node cloneGraph(Node node) {
             if(node==null) return null;
+            // returns node if seen, else add to map
             if(map.contains(node.val)) return map.get(node.val);
             Node newNode = new Node(node.val, new ArrayList<Node>());
             map.put(node.val, newNode);
+            // recursively call cloneGraph to add unseen nodes to neighbor list
             for(Node nn : node.neighbors) newNode.neighbors.add(cloneGraph(nn));
             return newNode;
         }
