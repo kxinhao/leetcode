41ab2fafa52d015adda492a16f4760f90b42d465
unknown <kxinhao@gmail.com>
2024-05-10 18:06:18 +0800

    1st impl of eval reverse polish notation

---
+++ EvalRPN.java
@@ -0,0 +1,41 @@
+/**
+ * LeetCode 150 Evaluate Reverse Polish Notation (Medium)
+ *
+ */
+class Solution {
+    public int evalRPN(String[] tokens) {
+        // valid operands +, -, *, /
+        List<String> operands = new ArrayList<String>();
+        operands.add("+");
+        operands.add("-");
+        operands.add("*");
+        operands.add("/");
+        // stack operations for order of operations, push digits pop operands
+        Stack<Integer> rpnStack = new Stack<Integer>();
+        tokenloop:
+        for(int i = 0; i<tokens.length; i++) {
+            if(operands.contains(tokens[i])) {
+                int a = rpnStack.pop();
+                int b = rpnStack.pop();
+                switch(tokens[i]) {
+                    case "+":
+                        rpnStack.push(b + a);
+                        break;
+                    case "-":
+                        rpnStack.push(b - a);
+                        break;
+                    case "*":
+                        rpnStack.push(b * a);
+                        break;
+                    case "/":
+                        rpnStack.push(b / a);
+                        break;
+                    default:
+                        System.out.println("something wrong");
+                }
+            }
+            else rpnStack.push(Integer.parseInt(tokens[i]));
+        }
+        return rpnStack.pop();
+    }
+}
