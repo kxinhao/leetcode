bb9a018b7387b741042074d89f78c97a263f3ae1
unknown <kxinhao@gmail.com>
2024-05-15 16:33:28 +0800

    refactoring

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -5,33 +5,35 @@
  * count edges (representing prereq courses) and subtract for each no-prereq course
  * and derivative course found, if 0 at the end, means schedule is possible
  */
+// 1st impl
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] goCourses = new List[numCourses]; // array to store list of prereq courses
+        List<Integer>[] inCourse = new List[numCourses]; // array to store list of prereq courses
         Queue<Integer> queue = new LinkedList<Integer>(); // stores courses with no prereq
         int[] incomingEdges = new int[numCourses]; // courses to be taken 
-        for(int i=0; i<goCourses.length; i++) {
-            goCourses[i] = new LinkedList<Integer>();
+        int edgeCnt = prerequisites.length;
+
+        for(int i=0; i<inCourse.length; i++) {
+            inCourse[i] = new LinkedList<Integer>();
         }
         // store prereq count for each course (pre[0]) in incomingEdges array
         // add course (pre[0]) to list of courses which have (pre[1]) as prereq
         for(int[] pre : prerequisites) {
             incomingEdges[pre[0]]++;
-            goCourses[pre[1]].add(pre[0]);
+            inCourse[pre[1]].add(pre[0]);
         }
         // add course to queue if no prerequisites
         for(int i = 0; i<incomingEdges.length;i++) {
             if(incomingEdges[i]==0) queue.add(i);
         }
         // count down from edges to check for cycle
-        // go through courses in queue to check if their linked courses result in completion
-        int edgeCnt = prerequisites.length;
+        // iterate through courses in queue to check if their linked courses result in completion
         while(!queue.isEmpty()) {
             int curr = queue.poll();
-            for(int go : goCourses[curr]) {
+            for(int in : inCourse[curr]) {
                 edgeCnt--;
                 // if course being checked has no further prereq, add to queue to replace prev
-                if(--incomingEdges[go]==0) queue.offer(go);
+                if(--incomingEdges[in]==0) queue.offer(in);
             }
         }
         // if edgeCnt is 0, means no further prereq needed for courses thus possible to complete
