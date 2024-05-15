83c3dfb6bd248face9f2fc2b6b2a6e3d0cd75f9a
unknown <kxinhao@gmail.com>
2024-05-15 16:04:02 +0800

    1st impl of best ans with comments

---
+++ CourseSchedule.java
@@ -0,0 +1,38 @@
+/**
+ * LeetCode 207 Course Schedule (Medium)
+ * Ensure no cycle so that the course list selection is completable
+ * BFS (best ans found so far)
+ */
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] goCourses = new List[numCourses]; // array to store list of prereq courses
+        Queue<Integer> queue = new LinkedList<Integer>(); // stores courses with no prereq
+        int[] incomingEdges = new int[numCourses]; // courses to be taken 
+        for(int i=0; i<goCourses.length; i++) {
+            goCourses[i] = new LinkedList<Integer>();
+        }
+        // store prereq count for each course (pre[0]) in incomingEdges array
+        // add course (pre[0]) to list for which array element goCourses is prereq to (pre[1])
+        for(int[] pre : prerequisites) {
+            incomingEdges[pre[0]]++;
+            goCourses[pre[1]].add(pre[0]);
+        }
+        // add course to queue if no prerequisites
+        for(int i = 0; i<incomingEdges.length;i++) {
+            if(incomingEdges[i]==0) queue.add(i);
+        }
+        // count down from edges to check for cycle
+        // go through courses in queue to check if their linked courses result in completion
+        int edgeCnt = prerequisites.length;
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+            for(int go : goCourses[curr]) {
+                edgeCnt--;
+                // if course being checked has no further prereq, add to queue to replace prev
+                if(--incomingEdges[go]==0) queue.offer(go);
+            }
+        }
+        // if edgeCnt is 0, means no further prereq needed for courses thus possible to complete
+        return edgeCnt==0;
+    }
+}
