8a70ef9de6cbcfbd79f66cc940379be0fd2f1da4
unknown <kxinhao@gmail.com>
2024-05-16 18:03:21 +0800

    added comments

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -1,17 +1,16 @@
 /**
  * LeetCode 207 Course Schedule (Medium)
  * Ensure no cycle so that the course list selection is completable
- * BFS (best ans found so far)
+ * BFS (best ans found so far) Topological sorting via Kahn's algorithm (partial, to check possibility)
  * count edges (representing prereqs) and subtract for each no-prereq course
  * and derivative course found, if 0 at the end, means schedule is possible
- * prereq [x,y] means to study x, y is prerequisite
  */
 // 2nd impl
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] inCourse = new List[numCourses]; // courses that follow array ind course 
-        Queue<Integer> queue = new LinkedList<Integer>(); // stores courses that can be taken
-        int[] incomingEdges = new int[numCourses]; // no of prereq courses for ind course 
+        List<Integer>[] inCourse = new List[numCourses]; // list of courses with ind as prereq
+        Queue<Integer> queue = new LinkedList<Integer>(); // stores courses with no prereq
+        int[] incomingEdges = new int[numCourses]; // prereq courses for ind crs 
         int edgeCnt = prerequisites.length;
 
         for(int i=0; i<inCourse.length; i++) {
@@ -21,17 +20,20 @@ class Solution {
         // add course (pre[0]) to list of courses which have (pre[1]) as prereq
         for(int[] pre : prerequisites) {
             incomingEdges[pre[0]]++;
-            inCourse[pre[1]].add(pre[0]); // for course pre[1], there is a following pre[0] 
+            inCourse[pre[1]].add(pre[0]);
         }
-        // add course to queue if no prerequisites
+        // add course to queue if no prerequisites (Kahn's algo)
         for(int i = 0; i<incomingEdges.length;i++) {
             if(incomingEdges[i]==0) queue.add(i);
         }
-        // count down from edges to check for cycle
-        // parese courses in queue to check if their linked courses result in completion
+
+        // partial Kahn's algorithm (without sorted list of nodes)
         while(!queue.isEmpty()) {
+            // remove course from queue which has no prereq 
             int curr = queue.poll();
+            // for each course that has prereq of curr course
             for(int in : inCourse[curr]) {
+                // -1 for each edge seen
                 edgeCnt--;
                 // if course being checked has no further prereq, add to queue to replace prev
                 if(--incomingEdges[in]==0) queue.offer(in);
