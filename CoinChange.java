56a947bf7e306cd7e4aaa51b2531593242946424
unknown <kxinhao@gmail.com>
2024-05-28 15:17:24 +0800

    1st impl coin change

---
+++ CoinChange.java
@@ -0,0 +1,22 @@
+/**
+ * LeetCode 322 Coin Change (Medium)
+ * DP
+ */
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        final int fillVal = 100000;
+
+        int dp = new int[amount+1]; // +1 for zero indexing
+        Arrays.fill(dp, fillVal); // fill dp array with unreachable val to indicate unseen/invalid
+
+        dp[0] = 0;
+
+        for(int sum = 1; sum<amount; sum++) {
+            for(int coin : coins) {
+                if(sum >= coin && dp[sum-coin] != fillVal) dp[sum] = Math.min(dp[i],dp[i-c]+1);
+            }
+        }
+        return dp[amount] == fillVal ? -1 : dp[amount];
+    }
+}
