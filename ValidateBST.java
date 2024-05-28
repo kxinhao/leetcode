5c50908d6d2153804104dbedfc58505dfc24ba04
unknown <kxinhao@gmail.com>
2024-05-28 19:12:39 +0800

    1st impl of validate bst using soln w/o min max Integer check

---
+++ ValidateBST.java
@@ -0,0 +1,30 @@
+/**
+ * LeetCode 98 Validate Binary Search Tree (Medium)
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return check(root, null, null);
+    }
+    boolean check(TreeNode node, Integer min, Integer max) {
+        if(node == null) return true;
+        if((min != null && node.val <= min) || (max!=null && node.val>=max)) return false;
+        return check(node.left, min, node.val) && check(node.right, node.val, max);
+    }
+}
