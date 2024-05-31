7166a21aae0f1f4515cf94ce61f56f88b3ff90ab
unknown <kxinhao@gmail.com>
2024-05-31 18:32:11 +0800

    2nd impl and added clarifying comments

--- ValidateBST.java
+++ ValidateBST.java
@@ -1,8 +1,12 @@
 /**
  * LeetCode 98 Validate Binary Search Tree (Medium)
- *
+ * Recursion with helper method that takes TreeNode, int min, int max input params
+ * helper method is initially called in main class, then recursively called by itself
+ * until all leaves have been checked or an invalidating node has been found
+ * recursion called on child nodes and using parent as min when checking right child lesser than
+ * and parent as max when checking left child larger than parent
  */
-// 1st impl without Integer max/min check
+// 2nd impl without Integer max/min check
 
 /**
  * Definition for a binary tree node.
@@ -23,9 +27,12 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         return check(root, null, null);
     }
-    boolean check(TreeNode node, Integer min, Integer max) {
+    private boolean check(TreeNode node, Integer min, Integer max) {
         if(node == null) return true;
+        // if right node is smaller than parent or left node is larger than parent, BST invalid
         if((min != null && node.val <= min) || (max!=null && node.val>=max)) return false;
+        // check left child using curr.val as max to ensure left child is smaller than curr
+        // check right child using curr.val as min to ensure right child is larger than curr
         return check(node.left, min, node.val) && check(node.right, node.val, max);
     }
 }
