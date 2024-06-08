e3f25dc7794c38e9b3fb15300d8f6cfb146d5d40
unknown <kxinhao@gmail.com>
2024-06-08 12:21:41 +0800

    added comments

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -12,11 +12,16 @@ class Solution {
         while (lo<=hi) {
             int mid = (lo+hi)/2;
             if(target==nums[mid]) return mid;
+            // nums[mid] belongs to the 1st part of nums
             if(nums[lo]<=nums[mid]) {
+                // target belongs to 2nd half of nums, reposition left pointer
                 if(target>nums[mid]||target<nums[lo]) lo = mid+1;
+                // target belongs to 1st half of nums, decrement right pointer
                 else hi = mid-1;
-            } else {
+            } else { // nums[mid] < nums[lo], nums[mid] belongs to 2nd part of nums
+                // target belongs to 1st half of nums, reposition right pointer
                 if(target<nums[mid]||target>nums[hi]) hi = mid-1;
+                // target belongs to 2nd half of nums, increment left pointer
                 else lo = mid+1;
             }
         }
