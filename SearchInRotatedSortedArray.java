38bc8bb88ddeebd44d81ecf15bd74973d7f1eea5
kxinhao <kxinhao@gmail.com>
2024-06-10 02:45:01 +0800

    added comments

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -1,10 +1,11 @@
 /**
  * LeetCode 33 Search in Rotated Sorted Array (Medium)
- * First determine nums[mid] belongs to 1st or 2nd half of rotated array
+ * First determine nums[mid] is in sorted or rotated part of array
  * then reposition lo and hi pointers based on comparison to target value
  * repeat until target appears at arr[mid] then return ind value
  */
 // binary search for TC O(logn)
+// eg [4,5,6,7,0,1,2], target = 0
 
 class Solution {
     public int search(int[] nums, int target) {
@@ -12,16 +13,17 @@ class Solution {
         while (lo<=hi) {
             int mid = (lo+hi)/2;
             if(target==nums[mid]) return mid;
-            // nums[mid] belongs to the 1st part of nums
-            if(nums[lo]<=nums[mid]) {
-                // target belongs to 2nd half of nums, reposition left pointer
+
+            // nums[mid] is in the sorted part of nums (7)
+            if(nums[lo]<=nums[mid]) { // 4<7, 0<1
+                // target is in 2nd half of nums, disregard 1st half (0<4, must be in right half)
                 if(target>nums[mid]||target<nums[lo]) lo = mid+1;
-                // target belongs to 1st half of nums, decrement right pointer
+                // target is in 1st half of nums, disregard 2nd half (0<1)
                 else hi = mid-1;
-            } else { // nums[mid] < nums[lo], nums[mid] belongs to 2nd part of nums
-                // target belongs to 1st half of nums, reposition right pointer
+            } else { // nums[mid] < nums[lo], nums[mid] is in rotated part of nums
+                // target is in 1st half of nums, disregard 2nd half
                 if(target<nums[mid]||target>nums[hi]) hi = mid-1;
-                // target belongs to 2nd half of nums, increment left pointer
+                // target is in 2nd half of nums, disregard 1st half
                 else lo = mid+1;
             }
         }
