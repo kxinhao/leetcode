800cca427756c0f2d3f91aad6554b67615293a80
unknown <kxinhao@gmail.com>
2024-06-10 23:35:42 +0800

    added soln for Combination Sum

---
+++ CombinationSum.java
@@ -0,0 +1,26 @@
+/**
+ * LeetCode 39 Combination Sum (Medium)
+ *
+ */
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        return combinate(list, new ArrayList<>(), candidates, target, 0);
+    }
+    private List<List<Integer>> combinate(List<List<Integer>> list, List<Integer> temp, int[] candidates, int rem, int start) {
+        if(rem<0) return list;
+        else if(rem==0) {
+            list.add(new ArrayList<>(temp));
+            return list;
+        } else {
+            for(int i = start; i<candidates.length; i++) {
+                temp.add(candidates[i]);
+                combinate(list, temp, candidates,  rem-candidates[i],i);
+                temp.remove(temp.size()-1);
+            }
+        }
+        return list;
+    }
+}
