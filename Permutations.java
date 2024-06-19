d3e69fe9d239161d62dee40ff9d974419d4b9223
unknown <kxinhao@gmail.com>
2024-06-19 21:13:09 +0800

    2nd impl, added comments

--- Permutations.java
+++ Permutations.java
@@ -3,7 +3,7 @@
  *
  */
 // eg. nums = [1,2,3]
-// 1st impl
+// 2nd impl
 
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
@@ -18,9 +18,19 @@ class Solution {
             return;
         }
         // interate through the positions in the nums array to populate same ind in ans list
+        // loop 0 to curr.size() for each recursion call
         for(int i=0; i<=curr.size(); i++) {
             curr.add(i, nums[ind]);
-            if(curr.size()>nums.length) continue;
+            // i=0, ind=0, curr=[1];
+            //                       i=0, ind=1, curr=[2,1]; i=0, ind=2, curr=[3,2,1]
+            //                                   curr=[2,1]; i=1, ind=2, curr=[2,3,1] 
+            //                                   curr=[2,1]; i=2, ind=2, curr=[2,1,3]
+            //                                   curr=[2,1];
+            //                                   curr=[1];
+            //                       i=1, ind=1, curr=[1,2]; i=0, ind=2, curr=[3,1,2]
+            //                                   curr=[1,2]; i=1, ind=2, curr=[1,3,2]
+            //                                   curr=[1,2]; i=2, ind=2, curr=[1,2,3]
+            if(curr.size()>nums.length) break;
             backtrack(nums, ind+1, ans, curr);
             curr.remove(i);
         }
