3b81dad5b27c619ed908016f44c1e7f06737c727
unknown <kxinhao@gmail.com>
2024-07-13 20:35:56 +0800

    String to Integer atoi from soln

---
+++ StringToInteger(atoi).java
@@ -0,0 +1,26 @@
+/**
+ * LeetCode 8 String to Integer (atoi) (Medium)
+ *
+ */
+
+class Solution {
+    public int myAtoi(String s) {
+        int index = 0, total = 0, sign = 1;
+        if(s.length()==0) return 0;
+        while(index<s.length() && s.charAt(index)==' ') index++;
+        if(index==s.length()) return total;
+        if(s.charAt(index)=='+'||s.charAt(index)=='-') {
+            sign = s.charAt(index) == '+' ? 1 : -1;
+            index++;
+        }
+        while(index<s.length()) {
+            int digit = s.charAt(index) - '0';
+            if(digit<0||digit>9) break;
+            if(Integer.MAX_VALUE/10 < total || Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE%10 < digit)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            total = total*10 + digit;
+            index++;
+        }
+        return total*sign;
+    }
+}
