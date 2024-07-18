43305020472798f2be9d6d7b5d4c9e0afe97cbd1
unknown <kxinhao@gmail.com>
2024-07-18 20:13:18 +0800

    2nd impl subsets, added comments

--- Subsets.java
+++ Subsets.java
@@ -2,6 +2,22 @@
  * LeetCode 78 Subsets (Medium)
  * recursion, TC: O(n^2)
  */
+// 2nd impl
+/*
+start = 0; ans = []; temp = [1]; i=1
+start = 1; ans = [],[1]; temp = [1,2]; i=2
+start = 2; ans = [],[1],[1,2]; temp = [1,2,3]; i=3
+start = 3; ans = [],[1],[1,2],[1,2,3]; i=3==nums.length, doesnt enter loop
+start = 2; ans = [],[1],[1,2],[1,2,3]; temp = [1,2]; start = 2 < nums.length, doesnt continue with loop
+start = 1; temp = [1], i = 2; temp = [1,3];
+start = 3; ans = [],[1],[1,2],[1,2,3],[1,3]
+start = 1; i=2; temp = [1]
+start = 0; temp = []; i=1; temp=[2]; i+1=2
+start = 2; ans = [],[1],[1,2],[1,2,3],[1,3],[2]; temp = [2,3]; i+1=3
+start = 3; ans = [],[1],[1,2],[1,2,3],[1,3],[2],[2,3]; i=3==nums.length, doesnt enter loop
+start = 2; temp = [2]
+start = 1; temp = []; i=2; temp = [3]; ans = [],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]
+*/
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
