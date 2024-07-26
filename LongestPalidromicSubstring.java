c172dfa901a3ee26c489855b61ba9e2548f842ce
unknown <kxinhao@gmail.com>
2024-07-26 17:18:28 +0800

    added comments

--- LongestPalidromicSubstring.java
+++ LongestPalidromicSubstring.java
@@ -1,7 +1,7 @@
 /**
  * LeetCode 5 Longest Palindromic Substring (Medium)
  * 2 pointer, TC: O(N^2), O(N) going through each char in s, O(N) substring operation after java7
- * Manacher's Algo (?)
+ * alternatively, Manacher's Algo TC: O(N)
  * naive/brute force is O(N^3), O(N^2) for every substring formation and palindrome check is O(N)
  */
 
