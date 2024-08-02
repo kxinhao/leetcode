db6440162e22c247a2fd77f78eb6e2236a2e2ed2
unknown <kxinhao@gmail.com>
2024-08-02 18:58:05 +0800

    3rd impl, added comments

--- ConstructBTFromPreorderAndInorderTraversal.java
+++ ConstructBTFromPreorderAndInorderTraversal.java
@@ -23,7 +23,7 @@
  *     }
  * }
  */
-// 2nd impl
+// 3rd impl
 class Solution {
     Map<Integer, Integer> inorderMap = new HashMap<>();
 
@@ -35,9 +35,14 @@ class Solution {
 
     private TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
         if(preStart > preEnd || inStart > inEnd) return null;
+        // subtree root node formed from preorder array preStart index element
         TreeNode root = new TreeNode(preorder[preStart]);
+        // subtree root index from inorder array
         int rootInd = inorderMap.get(root.val);
+        // num of left nodes derived from inorder array, root index of inorder arr - inorder start ind
         int leftNodes = rootInd - inStart;
+        // build left subtree by using preorder arr and taking next ind of preorder array to be start
+        // and preStart + num of left nodes as end ind (remainder of left nodes)
         root.left = build(preorder, preStart+1, preStart+leftNodes, inorder, inStart, rootInd-1);
         root.right = build(preorder, preStart+leftNodes+1, preEnd, inorder, rootInd+1, inEnd);
         return root;
