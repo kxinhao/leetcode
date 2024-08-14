1c1f86e43f3b6b8c7617c810db615b92b65f09ce
unknown <kxinhao@gmail.com>
2024-08-14 21:43:32 +0800

    1st commit for trap rain water

---
+++ TrappingRainWater.java
@@ -0,0 +1,30 @@
+/**
+ * LeetCode 42 Trapping Rain Water (Hard)
+ * TC: O(N), SC: O(1)
+ * single pass soln, 2 pointer l and r moving inward, calc vol at each point
+ */
+
+class Solution {
+    public int trap(int[] height) {
+        int rainWater = 0;
+        if(height==null || height.length<2) return 0;
+        int left = 0, right = height.length-1, maxLeft = 0, maxRight = 0;
+        int waterVol = 0;
+        while(left<right) {
+            if(height[left]<height[right]) {
+                if(height[left]>=maxLeft) maxLeft = height[left];
+                else {
+                    waterVol += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if(height[right]>=maxRight) maxRight = height[right];
+                else {
+                    waterVol += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return waterVol;
+    }
+}
