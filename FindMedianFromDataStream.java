cf730a6ed4930c8cdf7f48cbc162fd8ae7176a7c
unknown <kxinhao@gmail.com>
2024-08-21 19:09:43 +0800

    2nd impl 2heap

--- FindMedianFromDataStream.java
+++ FindMedianFromDataStream.java
@@ -13,6 +13,7 @@
  * ### Soln using TreeMap has addNum() TC: O(LogN) and findMedian() TC: O(1), SC: O(N)
  */
 
+// 2nd impl
 // 2x Heap soln (Best runtime 85 ms, additional addNum() conditions to shave 50ms off runtime)
 class MedianFinder {
 
