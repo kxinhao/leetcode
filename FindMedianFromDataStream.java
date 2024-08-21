f9fee84080042f0e9b93843b9febf51750981dab
unknown <kxinhao@gmail.com>
2024-08-21 18:44:09 +0800

    added soln for Find Median from Data Streams

---
+++ FindMedianFromDataStream.java
@@ -0,0 +1,105 @@
+/**
+ * LeetCode 295 Find Median from Data Stream (Hard)
+ * use 2 heap, min/max to store larger/smaller halves of data
+ * if thread-safe requirement, use PriorityBlockingQueue instead of PriorityQueue for heap impl
+ * addNum() TC: O(LogN) SC: O(N)
+ * findMedian() TC: O(1) SC: O(N)
+ *
+ * ## Special Note: Interviewer may demand use of BST / Self-balancing BST (AVL/Red-Black),
+ *                  added soln using Red-Black Tree Java Impl(TreeMap) and Doubly Linked-List
+ *                  TreeMap operations TC: O(LogN)
+ * ### Special Note2: Thread-safety can be attained via Synchronization by init TreeMap using
+ *                    SortedMap m = Collections.synchronizedSortedMap(new TreeMap());
+ * ### Soln using TreeMap has addNum() TC: O(LogN) and findMedian() TC: O(1), SC: O(N)
+ */
+
+// 2x Heap soln (Best runtime 85 ms)
+class MedianFinder {
+
+    private PriorityQueue<Integer> min; // minHeap, stores larger half, size n/2+1 if odd input
+    private PriorityQueue<Integer> max; // maxHeap, stores smaller half, size n/2
+
+    public MedianFinder() {
+        min = new PriorityQueue<>();
+        max = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        if(max.isEmpty()||num<max.peek()) max.add(num);
+        else {
+            min.add(num);
+        }
+        if(max.size()-min.size()>1) min.add(max.poll());
+        else if(min.size()-max.size()>1) {
+            max.add(min.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(min.size() == max.size()) return (max.peek()/2.0+min.peek()/2.0);
+        else if(min.size()>max.size()) return min.peek();
+        return max.peek();
+    }
+}
+
+// ## TreeMap and DLL Soln (Best runtime 128 ms)
+class MedianFinder {
+    class node{
+        int val;node prev;node next;
+        node(int v){val=v;prev=null;next=null;}
+    }
+    //will store median(if size is even it will store the first median)
+    node median;
+    int size;
+    //it will store dll in sorted form
+    node head;
+    //it will store last occurence of each unqiue integer in dll
+    TreeMap<Integer,node> h;
+    public MedianFinder() {
+        median=null;
+        size=0;
+        head=null;
+        h=new TreeMap();
+    }
+    
+    public void addNum(int num) {
+        size++;
+        node curr=new node(num);
+        //we find the last node starting from left whose value is less than num
+        if(h.lowerKey(num)!=null){
+            node key=h.get(h.lowerKey(num));
+            curr.next=key.next;
+            if(key.next!=null)key.next.prev=curr;
+            curr.prev=key;
+            key.next=curr;
+        }else{
+            if(head==null)head=curr;
+            else{
+                head.prev=curr;
+                curr.next=head;
+                head=curr;
+            }
+        }
+        //we will move median left or right depending upon num
+        if(h.get(num)==null)h.put(num,curr);
+        if(size==1||size==2)median=head;
+        else if(size==3)median=head.next;
+        else if(size%2==0){
+            if(median.val>=num)median=median.prev;
+        }else{
+            if(num>median.val)median=median.next;
+        }
+    }
+    
+    public double findMedian() {
+        if(size%2!=0)return (double)median.val;
+        else return (median.val+median.next.val)/((double)2);
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
