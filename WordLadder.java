820c8aaf66c81f9f5de8d817814eb76a7ece52cd
unknown <kxinhao@gmail.com>
2024-08-26 20:37:33 +0800

    added comments

--- WordLadder.java
+++ WordLadder.java
@@ -1,15 +1,18 @@
 /**
  * LeetCode 127 Word Ladder (Hard)
- * Double ended BFS
+ * Double ended BFS (forward and reverse search, ends when both ends meet)
  * cost of exploring each layer increases exponentially, so 2 half depth BFS is superior
  * to a single full depth BFS
- * TC: O(b^(d/2) + b^(d/2)) vs O(b^d) when doing single full length BFS
- * SC: O()
+ * TC: O(M^(N/2) + M^(N/2)) vs O(M^N) when doing single full length BFS
+ * M=length of word, N=wordList.size()
+ * simplified O(M^(N/2))
+ * SC: O(N)
  * /
 
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(!wordList.contains(endWord)) return 0;
+        // HashSet operations take O(1) due to hashing
         Set<String> dict = new HashSet<>(wordList), startSet = new HashSet<>(),
                     endSet = new HashSet<>(), visited = new HashSet<>();
         // init sets with start word and end word, initial addition of start word counts as 1
@@ -18,10 +21,11 @@ class Solution {
         endSet.add(endWord);
         int len = 1;
         
-        // while values present in either start or end Set, 
-        while(!startSet.isEmpty() && !endSet.isEmpty()) {
+        // while values present in either start Set, 
+        while(!startSet.isEmpty()) {
             // swap if startSet is larger then endSet so we will always be working with smaller set
             // so that the number of words to go through is minimized
+            // basically a reversal of search direction from the other point
             if(startSet.size()>endSet.size()) {
                 Set<String> tmp = startSet;
                 startSet = endSet;
@@ -29,11 +33,11 @@ class Solution {
             }
             // tmpSet to store valid target words formed and to replace in startSet
             Set<String> tmpSet = new HashSet<>();
-            // go through each word in dict(wordList) and 
+            // go through each word in startSet(aka transformation list) and 
             // find valid words that are 1 char distance away from the
             // start word taken from startSet and keep track of visited words in dict
             for(String word : startSet) {
-                char[] wordArr = word.toCharArray();
+                char[] wordArr = word.toCharArray(); // O(N) where N=word.length()
                 for(int i = 0; i<wordArr.length; i++) {
                     // to swap the char in word under consideration
                     char tmpChar = wordArr[i];
@@ -41,7 +45,7 @@ class Solution {
                         
                         // swap in new char and form the String
                         wordArr[i] = c;
-                        String target = new String(wordArr);
+                        String target = new String(wordArr); // O(N) where N=wordArr.length
                         // once endSet has the target word formed in this iteration of change,
                         // we can return the number of changes needed + this round of iteration
                         if(endSet.contains(target)) return len+1;
