b0a3b073a006058300e6987633f106a0226c39b2
unknown <kxinhao@gmail.com>
2024-08-30 20:07:03 +0800

    soln from ans

---
+++ MaximumProfitInJobScheduling.java
@@ -0,0 +1,62 @@
+/**
+ * LeetCode 1235 Maximum Profit in Job Scheduling (Hard)
+ * Dynamic Programing + Binary Search
+ * TC: O(N * logN)
+ * SC: O(N)
+ */
+
+class Solution {
+    class Job {
+        int start;
+        int end;
+        int profit;
+            
+        public Job(int start, int end, int profit) {
+            this.start = start;
+            this.end = end;
+            this.profit = profit;
+        }
+    }
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int len = profit.length;
+            
+        Job[] jobs = new Job[len];
+        for (int i = 0; i < len; i++) {
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
+        }
+        
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a.end, b.end));
+        
+        int[] dp = new int[len];
+        dp[0] = jobs[0].profit;
+        
+        for (int i = 1; i < len; i++) {
+            int start = jobs[i].start;
+            int left = 0;
+            int right = i - 1;
+            int res = -1;
+            
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if (jobs[mid].end <= start) {
+                    res = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            
+            int take = jobs[i].profit;
+            if (res != -1) {
+                take += dp[res];
+            }
+            
+            int nottake = dp[i - 1];
+            
+            dp[i] = Math.max(take, nottake);
+        }
+        
+        return dp[len - 1];
+    }
+}
