58c11cec1fe669e8ae6a33a25a18363399f2d9d1
unknown <kxinhao@gmail.com>
2024-09-05 19:53:27 +0800

    renamed return var for clarity, replaced comparison method with Integer.compare to prevent int overflow

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -16,15 +16,15 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
        if(lists==null||lists.length==0) return null; 
-       PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length, (a,b)->a.val-b.val);
-       ListNode start = new ListNode(0);
-       ListNode tail = start;
+       PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length, (a,b)->Integer.compare(a.val, b.val));
+       ListNode dummy = new ListNode(0);
+       ListNode tail = dummy;
        for(ListNode node:lists) if(node!=null) queue.add(node);
        while(!queue.isEmpty()) {
             tail.next = queue.poll();
             tail = tail.next;
             if(tail.next!=null) queue.add(tail.next);
        }
-       return start.next;
+       return dummy.next;
     }
 }
