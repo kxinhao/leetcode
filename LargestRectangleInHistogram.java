71aa40bf03af616ac6b87fb739d5569514e80b7f
unknown <kxinhao@gmail.com>
2024-09-14 20:31:29 +0800

    added nonstack and stack ans

---
+++ LargestRectangleInHistogram.java
@@ -0,0 +1,50 @@
+/**
+ * LeetCode 84 Largest Rectangle in Histogram (Hard)
+ * As rectangle can only be formed when the adjacent blocks are >= current block, we need to first find
+ * the previous and next smallest blocks from the current
+ * use stack to track
+ */
+
+// nonstack soln
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        int ans = 0;
+        for (int i = 0; i <= heights.length; i++) {
+            int cur = (i == heights.length) ? -1 : heights[i];
+            while (!stack.isEmpty() && heights[stack.peek()] >= cur) {
+                int h = heights[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                ans = Math.max(ans, h * w);
+            } 
+            stack.push(i);
+        }
+        return ans;
+    }
+}
+
+// stack soln
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= len;) {
+            int h = (i == len ? 0 : heights[i]);
+            if (stack.isEmpty() || h >= heights[stack.peek()]) {
+                stack.push(i);
+                i++;
+            }else {
+                int curHeight = heights[stack.pop()];
+                int rightBoundary = i - 1;
+                int leftBoundary = stack.isEmpty() ? 0 : stack.peek() + 1;
+                int width = rightBoundary - leftBoundary + 1;
+                maxArea = Math.max(maxArea, (curHeight * width));
+            }
+        }
+        return maxArea;
+    }
+}
