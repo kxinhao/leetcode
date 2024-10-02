c26cba0cb200034745dd36eca49f5876c68e731c
unknown <kxinhao@gmail.com>
2024-10-02 19:51:21 +0800

    refactored to use single array instead

--- RansomNote.java
+++ RansomNote.java
@@ -1,17 +1,19 @@
-// using array to store char occurance using char unicode as rep index, compare occurances
+/*
+ * LeetCode 383 Ransom Note (Easy)
+ * using array to store char occurance using char unicode as rep index, compare occurances
+ * TC: O(N), SC: O(1)
+ */
+
+// 2nd impl, using single arr 
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length()>magazine.length()) return false;
         int[] magArr = new int[26];
-        int[] rnArr = new int[26];
         for(char c : magazine.toCharArray()) {
             magArr[c - 'a']++;
         }
         for(char c : ransomNote.toCharArray()) {
-            rnArr [c-'a']++;
-        }
-        for(int i = 0; i<rnArr.length; i++) {
-            if(rnArr[i]>magArr[i]) return false;
+            if(--magArr[c-'a']<0) return false;
         }
         return true;
     }
