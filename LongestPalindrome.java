bb52a87e38012a43d88e6cb05d57ddd94657ca03
unknown <kxinhao@gmail.com>
2024-10-04 19:12:06 +0800

    refactored code and added comments

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,39 +1,28 @@
 /**
  * LeetCode 409 Longest Palindrome (Easy)
- *
+ * TC: O(N), SC: O(1)
  */
 
 // run through each letter, increment in array of lower/uppercase ascii representing indexes
 // longest possible palindrome calculated by summing up all even freq letters and 
 // odd freq letters -1 and add 1 back at end of calculation if odd number was present.
-// Time Complexity O(n)
-// Space Complexity O(1)
+// simulates adding characters to left and right ends to form palindrome, drop in 1 char with 
+// odd freq to middle at the end if they exist
 class Solution {
     public int longestPalindrome(String s) {
-        // a palindrome can only have 1 odd number letter freq count, result in odd length
         if(s.length()==1) return 1;
-        int[] letterFreq = new int[52];
-        for(int i = 0; i<s.length(); i++) {
-            if(s.codePointAt(i)<91) { // for uppercase letters
-                letterFreq[s.charAt(i) - 'A']++;
-            } else {// for lowercase letters
-                letterFreq[s.charAt(i) - 'a' + 26]++;
-            }
-        }
-        int pLength = 0;
+        int[] freq = new int[60];
+        int maxLength = 0;
         boolean hasOdd = false;
-        for(int i = 0; i<letterFreq.length; i++) {
-            // add letters with even freq directly
-            if(letterFreq[i]%2 == 0) {
-                pLength = pLength + letterFreq[i];
-            } else {
-                // add letters with odd freq and sub 1 to make even
+        for(char c : s.toCharArray()) freq[c-'A']++;
+        for(int charFreq : freq) {
+            if(charFreq%2==0) maxLength += charFreq;
+            else {
+                maxLength += charFreq-1;
                 hasOdd = true;
-                pLength = pLength+letterFreq[i]-1;
             }
         }
-        // add back 1 if odd numbered char freq present
-        if(hasOdd) return pLength+1;
-        return pLength;
+        if(hasOdd) return maxLength+1;
+        return maxLength;
     }
 }
