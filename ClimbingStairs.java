9095a82f53d541a2247f0a982427f92ce0c7c7b8
unknown <kxinhao@gmail.com>
2024-10-04 18:39:51 +0800

    modified soln to match fibonacci seq logic better and comments

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,19 +1,19 @@
 /**
  * LeetCode 70 Climbing Stairs (Easy)
+ * TC: O(N), SC: O(1)
  */
 
-// modified right shifted fibonacci sequence, n=1 and n=0 both result in 1 step taken 
-// to give n=2 total of 2 ways (2+0),(1+1);
-// uses 2 variables to track n-1 and n-2 to get n ways to climb stairs
+// fibonacci seq skipping 0 value due to constraints, track last 2 steps and start from step 3
+// updates last 2 step with each step taken, returns last step after it has been assigned curr
 class Solution {
     public int climbStairs(int n) {
-        if(n==1||n==0) return 1; // modified from fibonacci as n = 2 has 2 ways
-        int curr = 1, prev = 1; // base n-1, n-2, capped at n=2
-        for(int i = 2; i<=n; i++){
-            int tmp = curr; // store n-1 from prev loop to use as n-2 in next loop
-            curr = curr + prev; // n = n-1 + n-2
-            prev = tmp; // set n-2 for next calc
+        if(n==1) return 1;
+        int prev1 = 1, prev2 = 2;
+        for(int i = 3; i<=n; i++) {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
         }
-        return curr;
+        return prev2;
     }
 }
