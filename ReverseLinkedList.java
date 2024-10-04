0e1dfe0b0fe1209b175578f72fe2e2cf5dd3ce17
unknown <kxinhao@gmail.com>
2024-10-04 20:10:08 +0800

    added comments

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,3 +1,9 @@
+/*
+ * LeetCode 206 Reverse Linked List (Easy)
+ * Iterative TC: O(N), SC: O(1)
+ * Recursive TC: O(N), SC O(N)
+ */
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,21 +16,21 @@
  */
 class Solution {
     
-    //recursive method
-    public ListNode reverseList(ListNode node) {
-        if(node=null || node.next==null) return node; //checks if last node and returns
-        ListNode prev = null;
-        ListNode curr = reverseList(node.next); //travels down to tail node through recursion; node is 2nd last node here
-        node.next.next = node; //points last node to 2nd last by assigning last node's next'
-        node.next = prev; //break link from node to node.next as the reverse link has been established
-        return curr;
-    }
-
-    //iterative method
+    //recursive method (may cause call stack buffer overflow when large input is given)
     /*
     public ListNode reverseList(ListNode node) {
-        ListNode prev = null;
-        ListNode curr = node;
+        if(node=null || node.next==null) return node; //checks if last node and returns
+        ListNode curr = reverseList(node.next); //travels down to tail node through recursion; node is 2nd last node here
+        node.next.next = node; //points last node to 2nd last by assigning last node's next'
+        node.next = null; //break link from node to node.next as the reverse link has been established
+        return curr;
+    }
+    */
+
+    //iterative method
+    
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
 
         while(curr!=null) {
             ListNode next = curr.next;
@@ -32,7 +38,7 @@ class Solution {
             prev = curr; 
             curr = next;
         }
-        return curr;
+        return prev;
     }
-    */
+    
 }
