21ebd9631f0055492407ae293bfde8789d60a426
unknown <kxinhao@gmail.com>
2024-10-05 18:07:18 +0800

    added comments

--- MiddleOfLinkedList.java
+++ MiddleOfLinkedList.java
@@ -1,3 +1,8 @@
+/*
+ * LeetCode 876 Middle of Linked List (Easy)
+ * TC: O(N), SC: O(1)
+ */
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,6 +13,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// naive soln O(1.5N)
 class Solution {
     public ListNode middleNode(ListNode head) {
         int mid = 0, length = 1;
@@ -17,7 +24,7 @@ class Solution {
             length++;
         }
         mid = (int) Math.ceil(length/2.0);
-        // mid node referenced from prev node, hence loop stops before mid ind
+        // mid node referenced from prev node, hence loop must stop before mid ind
         for(int i = 1; i<mid; i++) {
             midNode = midNode.next;
         }
@@ -25,7 +32,8 @@ class Solution {
     }
 }
 
-// single pass 2 pointer soln
+// 2nd impl
+// single pass 2 pointer soln O(.5N)
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
