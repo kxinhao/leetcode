c99097027a78852cf7170b29f063cb4cc4e78d73
kxinhao <kxinhao@gmail.com>
2024-10-11 01:39:46 +0800

    4th impl and modified comments

--- InsertInterval.java
+++ InsertInterval.java
@@ -2,33 +2,9 @@
  * LeetCode 57 Insert Interval (Medium)
  * 3 cases to account for, pre intersect of newInterval, post intersect and intersecting intervals
  * ##brute force impl, failed at non overlap new interval test
- * 2nd try ans derived from solutions, using arraylist and single pass index tracking operations
- * 3rd soln with input mutation
  */
-class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> ans = new ArrayList<>();
-        int i = 0;
-        int start = newInterval[0];
-        int end = newInterval[1];
-        // add preintersect intervals (new interval starts after interval ends)
-        while (i<intervals.length && intervals[i][1]<newInterval[0]) ans.add(intervals[i++]);
-        // add intersecting intervals by merging to lowest start and highest end
-        // (interval starts before end of new interval)
-        while (i<intervals.length && intervals[i][0]<=newInterval[1]) {
-            start = Math.min(start,intervals[i][0]);
-            end = Math.max(end,intervals[i][1]);
-            i++;
-        }
-        ans.add(new int[]{start,end});
-        // add post intersect intervals
-        // (interval starts after end of new interval)
-        while(i<intervals.length) ans.add(intervals[i++]);
-        return ans.toArray(new int [ans.size()][2]);
-    }
-}
 
-// input mutation soln
+// 4th soln
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> ans = new ArrayList<>();
@@ -39,7 +15,7 @@ class Solution {
                 newInterval = interval;
             }
             // interval before newInterval, insert interval slot
-            else if(interval[1]<newInterval[0]) {
+            else if(newInterval[0]>interval[1]) {
                 ans.add(interval);
             }
             else {
@@ -49,10 +25,7 @@ class Solution {
                 newInterval[1] = Math.max(newInterval[1], interval[1]);
             }
         }
-
         ans.add(newInterval);
-
         return ans.toArray(new int[ans.size()][]);
-
     }
 }
