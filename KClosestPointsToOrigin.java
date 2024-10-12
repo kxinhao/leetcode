f99587389892d3597a780ea928d42ab86704e352
unknown <kxinhao@gmail.com>
2024-10-12 19:21:53 +0800

    2nd impl

--- KClosestPointsToOrigin.java
+++ KClosestPointsToOrigin.java
@@ -3,12 +3,14 @@
  * Euclidean distance between target and origin is sqrt(x*x+y*y) where x and y are target point coords
  * implement euclidean comparator with b-a instead of a-b to get max heap from priorityqueue instead 
  * of default min heap
+ * TC: O(NLogK), SC: O(K)
  */
 
+// 2nd impl
 class Solution {
   public int[][] kClosest(int[][] points, int k) {
-    int[] distArr = new int[points.length];
-    PriorityQueue<int[]>pq = new PriorityQueue<int[]>((a,b) -> b[0]*b[0] + b[1]*b[1] - a[0]*a[0] - a[1]*a[1]);
+    // simplified euclidean distance comparison, skipping sqrt
+    PriorityQueue<int[]>pq = new PriorityQueue<int[]>((a,b)->(b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]);
     for(int[] p : points) {
       pq.offer(p);
       if(pq.size()>k) pq.poll();
