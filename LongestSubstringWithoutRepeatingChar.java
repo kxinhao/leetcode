9a654a17ff709b2e8d074d0702646c518fbe05bb
unknown <kxinhao@gmail.com>
2024-10-15 18:50:23 +0800

    added comments and 3rd impl

--- LongestSubstringWithoutRepeatingChar.java
+++ LongestSubstringWithoutRepeatingChar.java
@@ -1,17 +1,26 @@
 /**
  * LeetCode 3 Longest SubString Without Repeating Characters (Medium)
  * dual pointers soln, increment left pointer to right of already mapped character
+ * to start new substring if not use prev left value
+ * remember to +1 to length calculations
+ * TC: O(N), SC: O(N)
  */
 
+// 3rd impl
 class Solution {
   public int lengthOfLongestSubstring(String s) {
     if(s.length()<2) return s.length;
     int maxLength = 0;
     HashMap<Character,Integer> map = HashMap<Character,Integer>();
-    for(int i=0, j=0; i<s.length();i++) {
-      if(map.containsKey(s.charAt(i))) j = Math.max(j,map.get(s.charAt(i))) + 1;
-      map.put(s.charAt(i),i);
-      maxLength = Math.max(maxLength, i-j+1);
+    for(int left=0, right=0; right<s.length();right++) {
+        // if map alr has right char, set left ind to 
+        // max of comparison between most recent occurence ind of char and left ind(new ss start)
+        // decides if substring continues from previous start, or starts from current index
+        if(map.containsKey(s.charAt(right))) left = Math.max(left,map.get(s.charAt(right))) + 1;
+        // 1st loop adds 0 pos character to hashmap as (char,0);
+        // same char alr in map will have ind position updated to new ind
+        map.put(s.charAt(right),right);
+        maxLength = Math.max(maxLength, right-left+1);
     }
     return maxLength;
   }
