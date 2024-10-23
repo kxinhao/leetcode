35f344f1db390435241fc1c64f31c7a286480ae5
unknown <kxinhao@gmail.com>
2024-10-23 20:24:27 +0800

    refactored comments and 9th impl

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -1,26 +1,26 @@
-// 8th solution (first pass L to R, then R to L; curr init to 1 on start of both runs to rep prod except self)
+/*
+ * LeetCode 238 Product of Array except self (Medium)
+ * TC: O(N) 2 loops iterating N times each
+ * SC: O(N)
+ * eg. input: [1,2,3,4] output: [24,12,8,6] 
+ */
+
+// 9th solution
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
-        // representing exclusion of leftmost element
-        int curr = 1;
-        // populate prod of left to right excluding curr element
+        // prefix and postfix of 1 to account for left and right edge cases
+        int prefix = 1, postfix = 1;
+        // pre[i] = pre[i-1] * ans[i-1];
         for(int i = 0; i < nums.length; i++) {
-            ans[i] = curr;  // first pass population of values, no mult
-            curr *= nums[i]; // prep for next ans[i] left to right
+            ans[i] = prefix;  // first pass population of values, no mult
+            prefix *= nums[i]; // calculation for next prefix -> ans[i] assignment
         }
-        // example: nums[1,2,3,4]
-        // L to R pass results ans[1,1,2,6]
-        // represents mult result of values to left of curr ind, excluding right of curr ind values 
-        curr = 1;
-        // layer population of right to left with existing ans arr, excluding curr elem
+        // suff[i] = suff[i+1] * ans[i+1]
         for(int i = nums.length-1; i>= 0; i--) {
-            ans[i] *= curr; // mult with curr as 2nd pass: 6x1=6, 2*4=8, 1*12=12, 1*24=24
-            // formulation of curr value for index i-1 calculation of ans[i]
-            curr *= nums[i]; // calc curr for 2nd pass R to L: 1*4=4, 4*3=12, 12*2=24, 24*1=24(not used)
+            ans[i] *= postfix; // mult with curr as 2nd pass: 6x1=6, 2*4=8, 1*12=12, 1*24=24
+            postfix *= nums[i]; // calc curr for 2nd pass R to L: 1*4=4, 4*3=12, 12*2=24, 24*1=24(not used)
         }
-        // R to L pass results ans[24,12,8,6]
         return ans;
-
     }
 }
