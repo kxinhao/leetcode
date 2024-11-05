fd945100644d79909311b2590ba46a62dd22c763
unknown <kxinhao@gmail.com>
2024-11-05 19:27:59 +0800

    3rd impl, refactored var names

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -2,10 +2,10 @@
  * Leetcode 56 Merge Intervals (Medium)
  * Sort by start val and merge
  * must sort to prevent any missed overlaps of intervals at end of array and ensure condition 
- * that prev[0]<=curr[0]
+ * that curr[0]<=interval[0]
  * TC O(N log(N)) log linear TC
  */
-// 2nd impl with lambda comparator from soln and merge conditions from prior qns
+// 3rd impl with lambda comparator from soln and merge conditions from prior qns
 // eg. [1,10] at [[2,3],[4,5],[6,7],[8,9],[1,10]]
 
 class Solution {
@@ -16,18 +16,18 @@ class Solution {
         // Integer.compare(i1[0],i2[0]) instead of i1[0] - i2[0] to prevent large neg values overflow
         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0],i2[0]));
         List<int[]> ans = new ArrayList<int[]>(intervals.length);
-        int[] prev = intervals[0];
-        ans.add(prev);
-        for(int[] curr : intervals) {
-            // prev end is larger than curr start means that prev and curr intersect
-            // merge the 2 intervals by taking the max of boths end replacing prev end
-            if(curr[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], curr[1]);
+        int[] curr = intervals[0];
+        ans.add(curr);
+        // curr end time will be compared for merge if subsequent intervals have start time before
+        for(int[] interval : intervals) {
+            // curr end is larger than interval start means that curr and interval intersect
+            if(interval[0] <= curr[1]) {
+                curr[1] = Math.max(curr[1], interval[1]);
             }
-            // if no intersect, assign curr to prev and add to ans list 
+            // if no intersect, add this interval to ans and consider for merges
             else {
-                prev = curr;
-                ans.add(prev);
+                curr = interval;
+                ans.add(curr);
             }
         }
         return ans.toArray(new int[ans.size()][2]);
