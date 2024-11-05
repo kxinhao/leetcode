f8724eb182460a123f4e9d44609956e6771c6257
unknown <kxinhao@gmail.com>
2024-11-05 18:05:09 +0800

    5th impl, switched to linkedlist for templist

--- CombinationSum.java
+++ CombinationSum.java
@@ -13,24 +13,22 @@
  * and you can choose at most target times (actually it's smaller than target),
  * so we can assume the worst-case time complexity is O(N^target).
  */
-// 4th impl **holy backtrack**
+// 5th impl **holy backtrack**
 // eg. cand = [2,3,6,7], target = 7
 
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(candidates);
-        return combinate(list, new ArrayList<>(), candidates, target, 0);
+        combinate(list, new LinkedList<>(), candidates, target, 0);
     }
 
-    private List<List<Integer>> combinate(List<List<Integer>> list, List<Integer> temp, 
+    private void combinate(List<List<Integer>> list, LinkedList<Integer> temp, 
         int[] candidates, int rem, int start) {
 
-        if(rem<0) return list;
-        else if(rem==0) {
-            list.add(new ArrayList<>(temp));
-            return list;
-        } else {
+        if(rem<0) return;
+        else if(rem==0) ans.add(new ArrayList<>(temp));
+        else {
             for(int i = start; i<candidates.length; i++) {
                 if(candidates[i]>rem) break;
                 // r2. temp = [2,2], rem = 3
@@ -42,9 +40,8 @@ class Solution {
                 temp.add(candidates[i]);
                 // checking how many of candidate[i] we can add, remove after iterating through
                 combinate(list, temp, candidates,  rem-candidates[i], i);
-                temp.remove(temp.size()-1);
+                temp.removeLast();
             }
         }
-        return list;
     }
 }
