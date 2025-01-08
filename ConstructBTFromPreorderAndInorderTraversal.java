dd6b7079f103b7a8806e3a9f76199634f644bdee
unknown <kxinhao@gmail.com>
2025-01-08 16:44:51 +0800

    kata

--- ConstructBTFromPreorderAndInorderTraversal.java
+++ ConstructBTFromPreorderAndInorderTraversal.java
@@ -2,24 +2,23 @@
  * LeetCode 105 Construct BT from Preorder and Inorder Traversal (Medium)
  * preorder traversal: root, left, right
  * inorder traversal: left, root, right
+ * Construction using principle that first node in preorder traversal is always subtree root,
+ * thus we build the BT subtree root by subtree root while shifting the context the subtree is in
  * TC: O(N), SC: O(N) using recursion. full traversal of inorder/preorder trees needed and space
  * needed is directly proportional to length of the arrays
  * preorder first element is always root, can be used to derive num of left/right nodes in inorder
  * beginning of right subtree for curr node in the preorder can be derived using:
  *   preStart + numsOnLeft + 1 (eg. 0+1+1=2)
  * where numsOnLeft = root - inStart
- * the binary tree is built  subtree by subtree using preorder array and preStart ind as reference
- * for the subtree root
- * and the left/right subtree construction is boxed in by preStart/preEnd and inStart/inEnd contexts
  * exceeding the context window means the child node is null
  */
 
-// 5th impl
+// 6th impl
 // eg. preorder = [3,9,20,15,7]
 //     inorder = [9,3,15,20,7]
 class Solution {
 
-    // for quick node index reference from value, can only be used due to constraint
+    // for quick node ind reference from value, can only be used due to constraint
     // preorder and inorder arrays all contain unique values
     Map<Integer, Integer> inorderMap = new HashMap<>();
 
@@ -46,6 +45,7 @@ class Solution {
         // for preorder, preStart+leftNodes+1 as start ind and preEnd as end ind
         // for inorder, rootInd+1 as start ind and inEnd as end ind
         // eg. root.right = build(preorder, 2, 4, inorder, 2, 4);
+        // all +/- 1 is to account for root node
         root.right = build(preorder, preStart+leftNodes+1, preEnd, inorder, rootInd+1, inEnd);
         return root;
     }
