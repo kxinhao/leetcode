37582d7452f84b3023377ec74d833d99ac25e3a2
unknown <kxinhao@gmail.com>
2025-01-10 16:49:10 +0800

    kata and added comments

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -2,15 +2,17 @@
  * LeetCode 11 Container with Most Water (Medium)
  * TC: O(N), SC: O(1)
  */
+// 2nd impl
 
 class Solution {
     public int maxArea(int[] height) {
         int vol = 0, left = 0, right = height.length-1;
         while(left<right) {
             int lHeight = height[left], rHeight = height[right];
-            int lower = Math.min(lHeight, rHeight);
-            int newVol = lower * (right-left);
+            int minHeight = Math.min(lHeight, rHeight);
+            int newVol = minHeight * (right-left);
             if(newVol>vol) vol = newVol;
+            // skip left and right that give less volume
             while(left<right && height[left]<=lower) left++;
             while(left<right && height[right]<=lower) right--;
         }
