7e0bef00fb13caf6963a8f165f54aefd9b75335b
unknown <kxinhao@gmail.com>
2025-02-10 16:07:21 +0800

    moved var to same line

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -9,8 +9,7 @@
 class Solution {
     public int trap(int[] height) {
         if(height==null || height.length<2) return 0;
-        int left = 0, right = height.length-1, maxLeft = 0, maxRight = 0;
-        int waterVol = 0;
+        int left = 0, right = height.length-1, maxLeft = 0, maxRight = 0, waterVol = 0;
         while(left<right) {
             if(height[left]<height[right]) {
                 if(height[left]>=maxLeft) maxLeft = height[left];
