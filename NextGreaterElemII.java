8c0c4b771a4e23985eafb520aa097ed1bfae6188
unknown <kxinhao@gmail.com>
2025-02-11 19:23:05 +0800

    added soln for Next Greater Element II

---
+++ NextGreaterElemII.java
@@ -0,0 +1,24 @@
+/*
+ * Leetcode 503 Next Greater Element II (Medium)
+ * Monotonic soln, trick to circular traversal is to loop twice
+ * TC: O(N), SC: O(N)
+ */
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        if(nums.length==1) return new int[]{-1};
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans, -1);
+        for(int j = 0; j<2; j++) {
+            for(int i = 0; i<nums.length; i++) {
+                while(!stack.isEmpty() && nums[stack.peek()]<nums[i]) {
+                    int stackTop = stack.pop();
+                    ans[stackTop] = nums[i];
+                }
+                stack.push(i);
+            }
+        }
+        return ans;
+    }
+}
